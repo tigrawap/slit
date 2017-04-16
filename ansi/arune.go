c6b0555d2c746a7b011085f794c9a3f1bb377a57
Anton Bykov <tigrawap@gmail.com>
2017-04-16 21:26:24 +0300

    Safer ansi control sequences parsing

--- ansi/arune.go
+++ ansi/arune.go
@@ -16,14 +16,11 @@ type RuneAttr struct {
 type Astring struct {
 	Runes []rune
 	Attrs []RuneAttr
-	//Fg    []uint8
-	//Bg    []uint8
-	//Style []uint8
 }
 
 //Returns new Astring, struct containing bytes converted to runes and ansi attributes per rune
 func NewAstring(src []byte) Astring {
-	var end int
+	var distance int
 	var attr RuneAttr
 	var r rune
 	//var b byte
@@ -35,19 +32,23 @@ func NewAstring(src []byte) Astring {
 		make([]RuneAttr, max, max),
 	}
 	ri := 0
+mainLoop:
 	for i := 0; i < len(rr); i++ {
 		r = rr[i]
 		if r == 27 && i != max-1 { // [27 91] is control sequence
 			if rr[i+1] == 91 {
-				// TODO: Should be sequence(regex?-based, m can be something else)
-				end = runes.IndexRune(rr[i+2:], 'm')
-				if end == -1 {
+				// TODO: For now ignoring all escape sequences other then styling
+				distance = runes.IndexRune(rr[i+2:], 'm')
+				if distance > 7 {
+					continue // 000;000 is maximal distance, if m is further - something went wrong
+				}
+				if distance == -1 {
 					i = i + 1
 					continue
 				}
 				attr.Fg, attr.Bg, attr.Style = 0, 0, 0
-				if end != 0 {
-					data := string(rr[i+2:i+2+end])
+				if distance != 0 {
+					data := string(rr[i+2:i+2+distance])
 					formats := strings.Split(data, ";")
 					for _, format := range formats {
 						// TODO: Can be optimized by using bytes directly
@@ -60,10 +61,14 @@ func NewAstring(src []byte) Astring {
 							} else if f != 0 {
 								attr.Style = uint8(f)
 							}
+						} else {
+							// If could not parse as string - we are reading it wrong
+							// Ignoring control sequence itself, but not ditching characters
+							continue mainLoop
 						}
 					}
 				}
-				i = i + 2 + end
+				i = i + 2 + distance
 				continue
 				// Control sequence
 			}
